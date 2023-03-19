8349c393796281fb581003781c3135fa9f181c85
Narine <115227487+narinearut@users.noreply.github.com>
2023-03-19 18:38:47 +0000

    hw6

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+
+function createGalleryItems (items) {
+    return items.map(({preview, original, description}) =>
+` <li class = ".gallery__item">
+        <a><img class = ".gallery__image" src="${preview}" alt="${description}">
+    </li>`
+    ) 
+}
