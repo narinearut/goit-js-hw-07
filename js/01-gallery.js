4e477dc1c31092e3380f92b25077c8ae7bc81bf6
Narine <115227487+narinearut@users.noreply.github.com>
2023-03-20 02:42:36 +0000

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItems(galleryItems);
+let modalSet = ""; //Потому, что в instance другая область видимости
 
-const galleryList = document.querySelector('.gallery');
+galleryList.insertAdjacentHTML("beforeend", itemsMarkup);
+galleryList.addEventListener("click", onClickItem);
 
+function createGalleryItems(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `       <li class = "gallery__item">
+<a class = "gallery__link" href="${original}">
+  <img 
+  class = "gallery__image" 
+  src="${preview}" 
+  alt="${description}"
+  data-source="${original}"
+  >
 
-function createGalleryItems (items) {
-    return items.map(({preview, original, description}) =>
-` <li class = ".gallery__item">
-        <a><img class = ".gallery__image" src="${preview}" alt="${description}">
-    </li>`
-    ) 
-}
+</a>
+</li>`
+    )
+    .join("");
+};
+
+function onClickItem(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  openCardModal(e.target.dataset.source);
+};
+
+function openCardModal(src) {
+  const instance = basicLightbox.create(`
+    <img src="${src}" width="800" height="600">
+`);
+  modalSet = instance;
+  modalSet.show();
+  window.addEventListener("keydown", onEscKeyPress);
+};
+
+function closeCardModal() {
+  modalSet.close();
+  window.removeEventListener("keydown", onEscKeyPress);
+};
+
+function onEscKeyPress(e) {
+  if (e.code === "Escape") {
+    closeCardModal();
+  }
+};
